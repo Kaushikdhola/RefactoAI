c242782838b535d3558045556d78c6296c4524a8
sshaji <sr569206@dal.ca>
2023-10-16 02:03:13 -0300

    feat: modified the clientID and clientSecret
    (cherry picked from commit c2143df89db072dec8f54246bd4ef8743c9bc114)

--- static/refactor-ui/src/store/reducer/index.ts
+++ static/refactor-ui/src/store/reducer/index.ts
@@ -1,9 +1,9 @@
 export const initialState = {
   isLoggedIn: JSON.parse(localStorage?.getItem("isLoggedIn") || "false") || false,
   user: JSON.parse(localStorage?.getItem("user") || "{}") || null,
-  client_id: process.env.REACT_APP_GITHUB_CLIENT_ID || "Iv1.31d036d7588db7bf",
+  client_id: process.env.REACT_APP_GITHUB_CLIENT_ID || "Iv1.59a0515e022cfba8",
   redirect_uri: window.location.origin,
-  client_secret: process.env.GITHUB_APP_SECRET || "4621fe47fc678eabcde035f7fad2b8a831130b9d",
+  client_secret: process.env.GITHUB_APP_SECRET || "c2591257e5a9b515f3111888bcb8a99c2df46674",
 };
 
 export const reducer = (state: any, action: any) => {
