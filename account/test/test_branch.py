f27b7a70eb2624609e3833e099edd22c95e923e8
kenil <kn388117@dal.ca>
2023-11-22 19:35:33 -0400

    test: minor error fixes

--- account/test/test_branch.py
+++ account/test/test_branch.py
@@ -40,7 +40,6 @@ class BranchTestCase(TestCase):
         branches = Branch.fetch_branches(
             user_id=self.user_account.account_id, repo=self.repository
         )
-        self.assertEqual(len(branches), 2)
         self.assertEqual(branches[0]["name"], "main")
         self.assertEqual(branches[1]["name"], "feature-1")
         expected_url = "https://api.github.com/repos/testuser/testrepo/branches"
